1eb2d9224cdc44dd2e27e31788e65238caeee195
Dimas_Karibov <dimaskaribov22@gmail.com>
2026-02-05 22:10:21 +0300

    Excessive files is deleted

--- lesson_2_task_2.py
+++
@@ -1,8 +0,0 @@
-def is_year_leap(year):
-    """Проверяет, является ли год високосным."""
-    return year % 4 == 0
-
-year_to_check = 2024
-is_leap = is_year_leap(year_to_check)
-
-print(f"Год {year_to_check}: {is_leap}")
--- lesson_2_task_3.py
+++
@@ -1,8 +0,0 @@
-import math
-def square(side):
-    """Возвращает площадь квадрата, округленную вверх, если сторона не целая."""
-    area = side * side
-    return math.ceil(area)
-side_length = 4.3 
-area_result = square(side_length)
-print(f"Площадь квадрата со стороной {side_length}: {area_result}")
