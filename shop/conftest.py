5d5d4e9a0242f03e02253540c84a289ab4ac26d2
Dimas_Karibov <dimaskaribov22@gmail.com>
2026-04-13 16:27:24 +0300

    Update conftest.py

--- shop/conftest.py
+++ shop/conftest.py
@@ -5,7 +5,6 @@ from selenium.webdriver.firefox.options import Options
 
 @pytest.fixture(scope="function")
 def driver():
-    """Фикстура для создания и закрытия драйвера Firefox"""
     options = Options()
     options.add_argument("--width=1920")
     options.add_argument("--height=1080")
