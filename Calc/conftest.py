bdd1962c454e6f35a5d00dd2e66fc5e0c196e101
Dimas_Karibov <dimaskaribov22@gmail.com>
2026-04-13 16:40:25 +0300

    Update conftest.py

--- Calc/conftest.py
+++ Calc/conftest.py
@@ -1,18 +1,18 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture(scope="function")
 def driver():
-    """Фикстура для создания и закрытия драйвера Firefox"""
     options = Options()
-    options.add_argument("--width=1920")
-    options.add_argument("--height=1080")
-
-    driver = webdriver.Firefox(options=options)
+    options.add_argument("--start-maximized")
+    # options.add_argument("--headless")  # Раскомментировать для headless режима
+    
+    driver = webdriver.Chrome(options=options)
     driver.implicitly_wait(5)
-
+    
     yield driver
-
+    
     driver.quit()
