7d1886920e20c879c2472461fe3e672063398603
Dimas_Karibov <dimaskaribov22@gmail.com>
2026-04-28 18:05:02 +0300

    Create db_config.py

---
+++ db_config.py
@@ -0,0 +1,10 @@
+# Мои параметры
+DB_CONFIG = {
+    'host': '127.0.0.1',
+    'port': '5432',
+    'database': 'QA-Testing',
+    'user': 'postgres',
+    'password': '12345'
+}
+
+CONNECTION_STRING = f"postgresql://postgres:12345@127.0.0.1:5432/QA-Testing"
