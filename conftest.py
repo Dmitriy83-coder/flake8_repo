fcab40453bf08d5b870ded161fdc8bc9c738c392
Dimas_Karibov <dimaskaribov22@gmail.com>
2026-04-30 18:45:04 +0300

    Create conftest.py

---
+++ conftest.py
@@ -0,0 +1,26 @@
+"""
+Фикстуры для pytest.
+"""
+import pytest
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+
+
+@pytest.fixture(scope="function")
+def driver():
+    """
+    Фикстура для создания и закрытия драйвера Firefox.
+
+    Returns:
+        webdriver.Firefox: Экземпляр драйвера Firefox
+    """
+    options = Options()
+    options.add_argument("--width=1920")
+    options.add_argument("--height=1080")
+
+    driver = webdriver.Firefox(options=options)
+    driver.implicitly_wait(5)
+
+    yield driver
+
+    driver.quit()
