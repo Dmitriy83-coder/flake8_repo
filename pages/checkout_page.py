5fbb79c1533b3b52e7aac86b79da91235bfd6420
Dimas_Karibov <dimaskaribov22@gmail.com>
2026-04-30 19:29:58 +0300

    Update checkout_page.py

--- pages/checkout_page.py
+++ pages/checkout_page.py
@@ -1,26 +1,59 @@
 """
-Page Object для страницы корзины.
+Page Object для страницы оформления заказа.
 """
 from selenium.webdriver.common.by import By
 from pages.base_page import BasePage
 
 
-class CartPage(BasePage):
+class CheckoutPage(BasePage):
     """
-    Страница корзины.
+    Страница оформления заказа.
 
     Локаторы:
-        CHECKOUT_BUTTON: Кнопка оформления заказа
+        FIRST_NAME_INPUT: Поле ввода имени
+        LAST_NAME_INPUT: Поле ввода фамилии
+        POSTAL_CODE_INPUT: Поле ввода почтового индекса
+        CONTINUE_BUTTON: Кнопка продолжения
+        TOTAL_LABEL: Метка с итоговой суммой
     """
 
-    # Локаторы (тип: tuple[By, str])
-    CHECKOUT_BUTTON = (By.CSS_SELECTOR, "[data-test='checkout']")
+    # Локаторы
+    FIRST_NAME_INPUT = (By.ID, "first-name")
+    LAST_NAME_INPUT = (By.ID, "last-name")
+    POSTAL_CODE_INPUT = (By.ID, "postal-code")
+    CONTINUE_BUTTON = (By.CSS_SELECTOR, "[data-test='continue']")
+    TOTAL_LABEL = (By.CLASS_NAME, "summary_total_label")
 
-    def click_checkout(self):
+    def enter_first_name(self, first_name: str) -> None:
+        """Ввести имя."""
+        self.send_keys(self.FIRST_NAME_INPUT, first_name)
+
+    def enter_last_name(self, last_name: str) -> None:
+        """Ввести фамилию."""
+        self.send_keys(self.LAST_NAME_INPUT, last_name)
+
+    def enter_postal_code(self, postal_code: str) -> None:
+        """Ввести почтовый индекс."""
+        self.send_keys(self.POSTAL_CODE_INPUT, postal_code)
+
+    def click_continue(self) -> None:
+        """Нажать кнопку Continue."""
+        self.click(self.CONTINUE_BUTTON)
+
+    def fill_checkout_form(self, first_name: str, last_name: str, postal_code: str) -> None:
+        """Заполнить форму оформления заказа."""
+        self.enter_first_name(first_name)
+        self.enter_last_name(last_name)
+        self.enter_postal_code(postal_code)
+        self.click_continue()
+
+    def get_total_amount(self) -> str:
         """
-        Нажать кнопку Checkout.
+        Получить итоговую сумму.
 
         Returns:
-            None
+            str: Итоговая сумма в формате "$XX.XX"
         """
-        self.click(self.CHECKOUT_BUTTON)
+        total_text = self.get_text(self.TOTAL_LABEL)
+        # Извлекаем только сумму из текста "Total: $58.29"
+        return total_text.replace("Total: ", "")
